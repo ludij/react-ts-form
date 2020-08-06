080e80c7cd0c52c260a10511fd9ade84ae840053
Luutzen Dijkstra <luutzen.dijkstra@avenga.com>
2020-08-06 18:11:40 +0200

    chore: cleaned up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from "@emotion/styled/macro"
-import logo from './logo.svg';
-import './App.css';
 
 const CButton = styled.button(`
   color: red;
@@ -9,23 +7,7 @@ const CButton = styled.button(`
 
 function App() {
   return (
-    <div className="App">
-      <CButton>test</CButton>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CButton>test</CButton>
   );
 }
 
